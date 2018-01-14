e348b7b2b2daa6206cfacfc6634d986c0275c5b0
petamoriken <moriken@kimamass.com>
2018-01-15 03:59:09 +0900

    fix :bug:

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,88 @@
-export type SimpleIDBKey = string | number | Date;
-export type SimpleIDBQuery = IDBKeyRange | SimpleIDBKey[] | null;
+export type SimpleIDBKey = string | number | Date | ArrayBuffer | SimpleIDBKeyArray;
+export type SimpleIDBInputKey = string | number | Date | ArrayBuffer | ArrayBufferView | SimpleIDBInputKeyArray;
+export type SimpleIDBQuery = IDBKeyRange | null;
+export type SimpleIDBCursorDirection = "next" | "nextunique" | "prev" | "prevunique";
 
 export interface SimpleIDBKeyAndValue {
     key: SimpleIDBKey,
     value: any,
 }
 
+export interface SimpleIDBKeyArray extends Array<SimpleIDBKey> {}
+export interface SimpleIDBInputKeyArray extends Array<SimpleIDBInputKey> {}
+
 export declare class SimpleIDB {
     constructor(dbName: string, dbVersion: number, onupgradeneeded: (this: IDBOpenDBRequest, event: IDBVersionChangeEvent) => any);
-    add(storeName: string, value: any, key?: SimpleIDBKey): Promise<SimpleIDBKey>;
-    get(storeName: string, key: SimpleIDBKey): Promise<any>;
-    update(storeName: string, value: any, key: SimpleIDBKey): Promise<void>;
-    delete(storeName: string, key: SimpleIDBKey): Promise<void>;
 
-    getAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: "next" | "nextunique" | "prev" | "prevunique"): Promise<SimpleIDBKeyAndValue[]>;
-    deleteAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: "next" | "nextunique" | "prev" | "prevunique"): Promise<void>;
-    getAndDeleteAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: "next" | "nextunique" | "prev" | "prevunique"): Promise<SimpleIDBKeyAndValue[]>;
-    clear(storeName: string): Promise<void>
+    /**
+     * ObjectStore に値を追加する。追加された key を返す
+     * @param storeName ObjectStore の名前
+     * @param value 追加する値
+     * @param key 追加する値のキー。ObjectStore が autoIncrement の場合は省略可能
+     */
+    add(storeName: string, value: any, key?: SimpleIDBInputKey): Promise<SimpleIDBKey>;
 
-    getObjectStores(storeName: string[], mode: "readonly" | "readwrite", oncomplete?: ((this: IDBTransaction, ev: Event) => any) | null, onerror?: ((this: IDBTransaction, ev: Event) => any) | null);
+    /**
+     * ObjectStore の値を追加もしくは更新する。追加もしくは更新された key を返す
+     * @param storeName ObjectStore の名前
+     * @param value 追加もしくは更新する値
+     * @param key 追加もしくは更新する値のキー
+     */
+    put(storeName: string, value: any, key: SimpleIDBInputKey): Promise<SimpleIDBKey>;
+
+    /**
+     * ObjectStore の値を取得する
+     * @param storeName ObjectStore の名前
+     * @param key 取得する値のキー
+     */
+    get(storeName: string, key: SimpleIDBInputKey): Promise<any>;
+
+    /**
+     * ObjectStore の値を削除する
+     * @param storeName ObjectStore の名前
+     * @param key 削除する値のキー
+     */
+    delete(storeName: string, key: SimpleIDBInputKey): Promise<void>;
+
+    /**
+     * ObjectStore の値を複数取得する
+     * @param storeName ObjectStore の名前
+     * @param query 取得する際のクエリ。null の場合全件取得する
+     * @param count 取得する値の個数の上限値
+     * @param direction 昇順、降順
+     */
+    getAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: SimpleIDBCursorDirection): Promise<SimpleIDBKeyAndValue[]>;
+
+    /**
+     * ObjectStore の値を複数削除する
+     * @param storeName ObjectStore の名前
+     * @param query 削除する際のクエリ。null の場合全件を表す
+     * @param count 削除する値の個数の上限値
+     * @param direction 昇順、降順
+     */
+    deleteAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: SimpleIDBCursorDirection): Promise<void>;
+
+    /**
+     * ObjectStore の値を複数取得し、同時に削除する
+     * @param storeName ObjectStore の名前
+     * @param query 取得、削除する際のクエリ。null の場合全件を表す
+     * @param count 取得、削除する値の個数の上限値
+     * @param direction 昇順、降順
+     */
+    getAndDeleteAll(storeName: string, query?: SimpleIDBQuery, count?: number, direction?: SimpleIDBCursorDirection): Promise<SimpleIDBKeyAndValue[]>;
+
+    /**
+     * ObjectStore の値を全件削除する
+     * @param storeName ObjectStore の名前
+     */
+    clear(storeName: string): Promise<void>;
+
+    /**
+     * ObjectStore を直接入手する。複雑なクエリを使う必要がある時に用いる
+     * @param storeNames ObjectStore の名前の配列
+     * @param mode IDBTransaction のモード
+     * @param oncomplete IDBTransaction が完了したときに呼ばれる callback
+     * @param onerror IDBTransaction が失敗したときに呼ばれる callback
+     */
+    getObjectStores(storeName: string[], mode?: "readonly" | "readwrite", oncomplete?: ((this: SimpleIDB) => any) | null, onerror?: ((this: SimpleIDB, error: DOMException) => any) | null): IDBObjectStore[];
 }
--- src/index.js
+++ src/index.js
@@ -19,14 +19,17 @@ export class SimpleIDB {
                 this.db = event.target.result;
                 resolve();
             };
-            request.onerror = reject;
+            request.onerror = (e) => reject(e.target.error);
         });
     }
 
     /**
      * ObjectStore に値を追加する。追加された key を返す
+     * @param storeName ObjectStore の名前
+     * @param value 追加する値
+     * @param key 追加する値のキー。ObjectStore が autoIncrement の場合は省略可能
      */
-    add(storeName, value, key) {
+    add(storeName, value, key = undefined) {
         return new Promise(async (resolve, reject) => {
             const store = await this.getObjectStore(storeName, "readwrite", () => resolve(key), reject);
             store.add(value, key).onsuccess = (event) => {
@@ -36,7 +39,24 @@ export class SimpleIDB {
     }
 
     /**
+     * ObjectStore の値を追加もしくは更新する。追加もしくは更新された key を返す
+     * @param storeName ObjectStore の名前
+     * @param value 追加もしくは更新する値
+     * @param key 追加もしくは更新する値のキー
+     */
+    put(storeName, value, key) {
+        return new Promise(async (resolve, reject) => {
+            const store = await this.getObjectStore(storeName, "readwrite", () => resolve(key), reject);
+            store.put(value, key).onsuccess = (event) => {
+                key = event.target.result;
+            };
+        });
+    }
+
+    /**
      * ObjectStore の値を取得する
+     * @param storeName ObjectStore の名前
+     * @param key 取得する値のキー
      */
     get(storeName, key) {
         return new Promise(async (resolve, reject) => {
@@ -49,37 +69,27 @@ export class SimpleIDB {
     }
 
     /**
-     * ObjectStore の値を更新する
-     */
-    update(storeName, value, key) {
-        return new Promise(async (resolve, reject) => {
-            const store = await this.getObjectStore(storeName, "readwrite", () => resolve(), reject);
-
-            store.openCursor(key).onsuccess = (event) => {
-                const cursor = event.target.result;
-                if (cursor == null) { reject(new RangeError(`Invalid key: ${ key }`)); }
-
-                cursor.update(value);
-            }
-        });
-    }
-
-    /**
      * ObjectStore の値を削除する
+     * @param storeName ObjectStore の名前
+     * @param key 削除する値のキー
      */
     delete(storeName, key) {
         return new Promise(async (resolve, reject) => {
-            const store = await this.getObjectStore(storeName, "readwrite", () => resolve(), reject);
+            const store = await this.getObjectStore(storeName, "readwrite", resolve, reject);
             store.delete(key);
         });
     }
 
     /**
      * ObjectStore の値を複数取得する
+     * @param storeName ObjectStore の名前
+     * @param query 取得する際のクエリ。null の場合全件取得する
+     * @param count 取得する値の個数の上限値
+     * @param direction 昇順、降順
      */
     getAll(storeName, query = null, count = Infinity, direction = "next") {
         return new Promise(async (resolve, reject) => {
-            let ret = [];
+            const ret = [];
             const store = await this.getObjectStore(storeName, "readonly", () => resolve(ret), reject);
 
             let index = 0;
@@ -98,16 +108,23 @@ export class SimpleIDB {
 
     /**
      * ObjectStore の値を複数削除する
+     * @param storeName ObjectStore の名前
+     * @param query 削除する際のクエリ。null の場合全件を表す
+     * @param count 削除する値の個数の上限値
+     * @param direction 昇順、降順
      */
     deleteAll(storeName, query = null, count = Infinity, direction = "next") {
         return new Promise(async (resolve, reject) => {
-            const store = await this.getObjectStore(storeName, "readwrite", () => resolve(), reject);
+            const store = await this.getObjectStore(storeName, "readwrite", resolve, reject);
 
-            if (query !== null && count === Infinity) {
-                store.delete(query);
+            if (count === Infinity) {
+                if (query === null) {
+                    store.clear();
+                } else {
+                    store.delete(query);
+                }
             } else {
                 let index = 0;
-
                 store.openCursor(query, direction).onsuccess = (event) => {
                     const cursor = event.target.result;
                     if (cursor == null) { return; }
@@ -124,10 +141,14 @@ export class SimpleIDB {
 
     /**
      * ObjectStore の値を複数取得し、同時に削除する
+     * @param storeName ObjectStore の名前
+     * @param query 取得、削除する際のクエリ。null の場合全件を表す
+     * @param count 取得、削除する値の個数の上限値
+     * @param direction 昇順、降順
      */
     getAndDeleteAll(storeName, query = null, count = Infinity, direction = "next") {
         return new Promise(async (resolve, reject) => {
-            let ret = [];
+            const ret = [];
             const store = await this.getObjectStore(storeName, "readwrite", () => resolve(ret), reject);
 
             let index = 0;
@@ -146,19 +167,24 @@ export class SimpleIDB {
     }
 
     /**
-     * ObjectStore の値を全て削除する
+     * ObjectStore の値を全件削除する
+     * @param storeName ObjectStore の名前
      */
     clear(storeName) {
         return new Promise(async (resolve, reject) => {
-            const store = await this.getObjectStore(storeName, "readwrite", () => resolve(), reject);
+            const store = await this.getObjectStore(storeName, "readwrite", resolve, reject);
             store.clear();
         });
     }
 
     /**
-     * ObjectStore を複数取得する。複雑なクエリを送る必要がある時に使う
+     * ObjectStore を直接入手する。複雑なクエリを使う必要がある時に用いる
+     * @param storeNames ObjectStore の名前の配列
+     * @param mode IDBTransaction のモード
+     * @param oncomplete IDBTransaction が完了したときに呼ばれる callback
+     * @param onerror IDBTransaction が失敗したときに呼ばれる callback
      */
-    async getObjectStores(storeNames, mode = "readonly", completeCallback = null, errorCallback = null) {
+    async getObjectStores(storeNames, mode = "readonly", completeCallback, errorCallback) {
         const transaction = await this.getTransaction(storeNames, mode, completeCallback, errorCallback);
         return storeNames.map(storeName => transaction.objectStore(storeName));
     }
@@ -166,7 +192,7 @@ export class SimpleIDB {
     /**
      * @private
      */
-    async getObjectStore(storeName, mode = "readonly", completeCallback = null, errorCallback = null) {
+    async getObjectStore(storeName, mode, completeCallback, errorCallback) {
         const transaction = await this.getTransaction(storeName, mode, completeCallback, errorCallback);
         return transaction.objectStore(storeName);
     }
@@ -177,8 +203,8 @@ export class SimpleIDB {
     async getTransaction(storeName, mode, completeCallback, errorCallback) {
         await this.loaded;
         const transaction = this.db.transaction(storeName, mode);
-        transaction.oncomplete = completeCallback;
-        transaction.onerror = errorCallback;
+        if (completeCallback != null) { transaction.oncomplete = () => completeCallback(); }
+        if (errorCallback != null) { transaction.onerror = (e) => errorCallback(e.target.error); }
         return transaction;
     }
 }
